8db775440bd2c52d8172194a83c99336fdad4118
Ilya Mafov <meat.100@gmail.com>
2020-05-25 23:48:45 +0300

    Keep screen on add

--- android/src/ru/spaceshooter/AndroidLauncher.java
+++ android/src/ru/spaceshooter/AndroidLauncher.java
@@ -1,6 +1,7 @@
 package ru.spaceshooter;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -12,5 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new SpaceShooter(), config);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 }
