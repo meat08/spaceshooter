77b27551c57c2ffe5a120455dfe45fe50b2af577
Ilya Mafov <meat.100@gmail.com>
2020-05-25 19:25:19 +0300

    Add animate button

--- core/src/ru/spaceshooter/sprite/ButtonNewGame.java
+++ core/src/ru/spaceshooter/sprite/ButtonNewGame.java
@@ -8,7 +8,11 @@ import ru.spaceshooter.screen.GameScreen;
 
 public class ButtonNewGame extends ScaledButton {
 
+    private static final float ANIMATE_INTERVAL = 0.8f;
+
     private GameScreen gameScreen;
+    private float animateTimer;
+    private boolean scaleUP = true;
 
     public ButtonNewGame(TextureAtlas atlas, GameScreen gameScreen) {
         super(atlas.findRegion("button_new_game"));
@@ -24,7 +28,16 @@ public class ButtonNewGame extends ScaledButton {
 
     @Override
     public void update(float delta) {
-        super.update(delta);
+        animateTimer += delta;
+        if (animateTimer >= ANIMATE_INTERVAL) {
+            animateTimer = 0f;
+            scaleUP = !scaleUP;
+        }
+        if (scaleUP) {
+            setScale(getScale() + 0.003f);
+        } else {
+            setScale(getScale() - 0.003f);
+        }
     }
 
     @Override
