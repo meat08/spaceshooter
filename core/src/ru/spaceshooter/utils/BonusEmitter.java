b7a40b7db5ecfc02aebd3c5792412fd4b30252c3
Ilya Mafov <meat.100@gmail.com>
2020-06-15 17:41:53 +0300

    Bonus and boss update Nuke bonus add Add new ship sprite (with double guns) Add save fields. Old save incompatible. Add enemy boss Double/triple gun (mani ship shoot two/three bullets permanent)

--- core/src/ru/spaceshooter/utils/BonusEmitter.java
+++ core/src/ru/spaceshooter/utils/BonusEmitter.java
@@ -9,25 +9,30 @@ import ru.spaceshooter.pool.BonusPool;
 public class BonusEmitter {
 
     private static final float GENERATE_INTERVAL = 12f;
+    private static final float GENERATE_NUKE_INTERVAL = 60f;
 
     private final TextureRegion[] hpBonusRegion;
     private final TextureRegion[] bulletBonusRegion;
     private final TextureRegion[] shieldBonusRegion;
+    private final TextureRegion[] nukeBonusRegion;
 
     private BonusPool bonusPool;
     private Ship ship;
     private float generateTimer;
+    private float generateNukeTimer;
 
     public BonusEmitter (TextureAtlas atlas, BonusPool bonusPool, Ship ship) {
         this.hpBonusRegion = Regions.split(atlas.findRegion("bonusHeal"),1, 6, 6);
         this.bulletBonusRegion = Regions.split(atlas.findRegion("bonusGuns"),1, 6, 6);
         this.shieldBonusRegion = Regions.split(atlas.findRegion("bonusShield"),1, 6, 6);
+        this.nukeBonusRegion = Regions.split(atlas.findRegion("bonusNuke"),1, 6, 6);
         this.bonusPool = bonusPool;
         this.ship = ship;
     }
 
     public void generate (float delta) {
         generateTimer += delta;
+        generateNukeTimer += delta;
         if (generateTimer >= GENERATE_INTERVAL) {
             generateTimer = 0f;
             float type = (float) Math.random();
@@ -39,6 +44,13 @@ public class BonusEmitter {
                 if (ship.getHp() < ship.getMaxHp()) {
                     bonusPool.obtain().set(hpBonusRegion, 1);
                 }
+            }
+        }
+        if (generateNukeTimer >= GENERATE_NUKE_INTERVAL) {
+            generateNukeTimer = 0f;
+            float chance = (float) Math.random();
+            if (chance > 0.7f) {
+                bonusPool.obtain().set(nukeBonusRegion, 4);
             }
         }
     }
