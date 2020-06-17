20384a4440388d457fdf044a6935b47c82b5e010
Ilya Mafov <meat.100@gmail.com>
2020-06-17 16:25:01 +0300

    Added desktop icon

--- desktop/src/ru/spaceshooter/desktop/DesktopLauncher.java
+++ desktop/src/ru/spaceshooter/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package ru.spaceshooter.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.spaceshooter.SpaceShooter;
@@ -10,6 +11,7 @@ public class DesktopLauncher {
 		config.width = 700;
 		config.height = 900;
 		config.resizable = false;
+		config.addIcon("textures/ic_launcher.png", Files.FileType.Internal);
 		new LwjglApplication(new SpaceShooter(), config);
 	}
 }
